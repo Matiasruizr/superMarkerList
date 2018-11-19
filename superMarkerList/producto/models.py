1ee686e826172d1cf36449484c1ee0ea42f44ccf
Matias <matiasruizr@hotmail.es>
2018-11-18 20:26:21 -0400

    Modelo tiendas

--- superMarkerList/producto/models.py
+++ superMarkerList/producto/models.py
@@ -1,3 +1,18 @@
-from django.db import models
-
 # Create your models here.
+from django.db import models
+from django.utils import timezone
+# Create your models here.
+
+
+class Product(models.Model):
+    # id_producto = models.CharField(max_length=200)
+    ubicacion = models.CharField(max_length=200)
+    ciudad = models.CharField(max_length=200)
+    region = models.CharField(max_length=200)
+    aprobado =  models.BooleanField
+    region = (
+        ('Rescatado', 'Rescatado'),
+        ('Disponible', 'Disponible'),
+        ('Adoptado', 'Adoptado'),
+    )
+    estado = models.CharField(max_length=10, choices=estado)
--- superMarkerList/tienda/models.py
+++ superMarkerList/tienda/models.py
@@ -1,3 +1,19 @@
-from django.db import models
-
 # Create your models here.
+from django.db import models
+from django.utils import timezone
+# Create your models here.
+
+
+class Tienda(models.Model):
+    # id_tienda = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=200)
+    ubicacion = models.CharField(max_length=200)
+    ciudad = models.CharField(max_length=200)
+    region = models.CharField(max_length=200)
+    aprobado =  models.BooleanField
+    region = (
+        ('Rescatado', 'Rescatado'),
+        ('Disponible', 'Disponible'),
+        ('Adoptado', 'Adoptado'),
+    )
+    estado = models.CharField(max_length=10, choices=estado)
