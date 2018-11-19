4b39aff93c0871d4de14e430b6c23d34026ccbf4
Matias <matiasruizr@hotmail.es>
2018-11-18 20:56:23 -0400

    Modelo Productos

--- superMarkerList/producto/models.py
+++ superMarkerList/producto/models.py
@@ -1,18 +1,17 @@
 # Create your models here.
 from django.db import models
 from django.utils import timezone
+from django import forms
 # Create your models here.
 
 
 class Product(models.Model):
     # id_producto = models.CharField(max_length=200)
-    ubicacion = models.CharField(max_length=200)
-    ciudad = models.CharField(max_length=200)
-    region = models.CharField(max_length=200)
-    aprobado =  models.BooleanField
-    region = (
-        ('Rescatado', 'Rescatado'),
-        ('Disponible', 'Disponible'),
-        ('Adoptado', 'Adoptado'),
-    )
-    estado = models.CharField(max_length=10, choices=estado)
+    nombre_producto = models.CharField(max_length=200)
+    costo_presupuestado = models.IntegerField(default=0)
+    costo_real = models.IntegerField(default=0)
+    id_tienda = models.CharField(max_length=200)
+    comentarios = models.CharField(max_length=200)
+    unidad =  models.IntegerField(default=0)
+    aprobado =  forms.BooleanField
+    descripcion = models.CharField(max_length=200)
--- superMarkerList/tienda/models.py
+++ superMarkerList/tienda/models.py
@@ -9,11 +9,24 @@ class Tienda(models.Model):
     nombre = models.CharField(max_length=200)
     ubicacion = models.CharField(max_length=200)
     ciudad = models.CharField(max_length=200)
-    region = models.CharField(max_length=200)
     aprobado =  models.BooleanField
     region = (
-        ('Rescatado', 'Rescatado'),
-        ('Disponible', 'Disponible'),
-        ('Adoptado', 'Adoptado'),
+        ('Region de Arica y Parinacota', 'Arica'),
+        ('Region de Tarapaca', 'Iquique'),
+        ('Region de Antofagasta', 'Antofagasta'),
+        ('Region de Atacama', 'Copiapo'),
+        ('Region de Coquimbo ', 'La Serena'),
+        ('Region de Valparaiso', 'Valparaiso'),
+        ('Region Metropolitana de Santiago', 'Santiago'),
+        ('Region del Libertador General Bernardo OHiggins', 'Rancagua'),
+        ('Region del Maule', 'Talca'),
+        ('Region de Ñuble', 'Chillan'),
+        ('Region del Biobio', 'Concepcion'),
+        ('Region de la Araucania', 'Temuco'),
+        ('Region de Los Rios', 'Valdivia'),
+        ('Region de Los Lagos', 'Puerto Montt'),
+        ('Region de Aysen', 'Coyhaique'),
+        ('Region de Magallanes y de la Antartica Chilena', 'Punta Arenas'),
     )
-    estado = models.CharField(max_length=10, choices=estado)
+    region = models.CharField(max_length=200)
+  
